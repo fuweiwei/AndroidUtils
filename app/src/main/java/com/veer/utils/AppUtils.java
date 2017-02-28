705cd9510c052382ed5f27a9b84e16a08656d566
fuweiwei <fuweiwei@kjtpay.com.cn>
2017-02-28 15:33:57 +0800

    Initial commit

---
+++ app/src/main/java/com/veer/utils/AppUtils.java
@@ -0,0 +1,96 @@
+package com.veer.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+/**
+ * APP辅助工具类
+ * Created by Veer
+ * date 17/2/28.
+ */
+
+public class AppUtils {
+
+    private AppUtils()
+    {
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+
+    }
+
+    /**
+     * 获取应用程序名称
+     */
+    public static String getAppName(Context context)
+    {
+        try
+        {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(
+                    context.getPackageName(), 0);
+            int labelRes = packageInfo.applicationInfo.labelRes;
+            return context.getResources().getString(labelRes);
+        } catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * [获取应用程序版本名称信息]
+     *
+     * @param context
+     * @return 当前应用的版本名称
+     */
+    public static String getVersionName(Context context)
+    {
+        try
+        {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(
+                    context.getPackageName(), 0);
+            return packageInfo.versionName;
+
+        } catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 打卡软键盘
+     *
+     * @param mEditText
+     *            输入框
+     * @param mContext
+     *            上下文
+     */
+    public static void openKeybord(EditText mEditText, Context mContext)
+    {
+        InputMethodManager imm = (InputMethodManager) mContext
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(mEditText, InputMethodManager.RESULT_SHOWN);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+                InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    /**
+     * 关闭软键盘
+     *
+     * @param mEditText
+     *            输入框
+     * @param mContext
+     *            上下文
+     */
+    public static void closeKeybord(EditText mEditText, Context mContext)
+    {
+        InputMethodManager imm = (InputMethodManager) mContext
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+}
